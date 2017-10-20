9f234a97b9fc87063996d17285f5341f93148863
Deblin Bagchi <deblinbagchi@gmail.com>
2017-10-20 17:23:49 -0400

    pushing delta_feats.py

--- delta_feats.py
+++ delta_feats.py
@@ -8,7 +8,10 @@ Date: Fall 2017
 import tensorflow as tf
 import numpy as np
 import random
-
+import os
+import struct
+ 
+from data_io import smart_open 
 #assert order >=0 && order <=2 , "invalid order specified"
 #assert window >=0 && window < 1000, "invalid window specified"
 order=2
@@ -55,44 +58,52 @@ def Process(input_feats,frame):
                 output = output + (sscale * input_feats[offset_frame][:])
         output_frame.append(output)
     result = np.stack(output_frame).flatten()
-    print(result.shape)
+    return result
 
+def process_data(ark_base_dir=os.getcwd(), scp_fn="data-fbank/train_si84_noisy/feats.scp"):
+    ark_dict = {}
+    totframes = 0
+    lines = 0
+    with open(scp_fn) as f:
+        for line in f:
+            lines = lines + 1
+            if lines>10:
+                break
+            if line == "":
+                continue
+            utt_id, path_pos = line.replace("\n", "").split()
+            ark_path, pos = path_pos.split(":")
+            ark_path = os.path.join(ark_base_dir, ark_path)
+            ark_read_buffer = smart_open(ark_path, "rb")
+            ark_read_buffer.seek(int(pos),0)
+            header = struct.unpack("<xcccc", ark_read_buffer.read(5))
+            #assert header[0] == "B", "Input .ark file is not binary"
+            rows = 0
+            cols = 0
+            m,rows = struct.unpack("<bi", ark_read_buffer.read(5))
+            n,cols = struct.unpack("<bi", ark_read_buffer.read(5))
+            tmp_mat = np.frombuffer(ark_read_buffer.read(rows*cols*4), dtype=np.float32)
+            utt_mat = np.reshape(tmp_mat, (rows, cols))
+            #utt_mat_list=utt_mat.tolist()
+            ark_read_buffer.close()
+            ark_dict[utt_id] = utt_mat
 
-scp_fn="data-fbank/train_si84_delta_noisy/feats.scp"
-            
-ark_dict = {}
-totframes = 0
-lines = 0
-with open(scp_fn) as f:
-    for line in f:
-        lines = lines + 1
-        if lines<=uid:
-            continue
-        if line == "":
-            continue
-        utt_id, path_pos = line.replace("\n", "").split()
-        ark_path, pos = path_pos.split(":")
-        ark_path = os.path.join(ark_base_dir, ark_path)
-        ark_read_buffer = smart_open(ark_path, "rb")
-        ark_read_buffer.seek(int(pos),0)
-        header = struct.unpack("<xcccc", ark_read_buffer.read(5))
-        #assert header[0] == "B", "Input .ark file is not binary"
-        rows = 0
-        cols = 0
-        m,rows = struct.unpack("<bi", ark_read_buffer.read(5))
-        n,cols = struct.unpack("<bi", ark_read_buffer.read(5))
-        tmp_mat = np.frombuffer(ark_read_buffer.read(rows*cols*4), dtype=np.float32)
-        utt_mat = np.reshape(tmp_mat, (rows, cols))
-        #utt_mat_list=utt_mat.tolist()
-        ark_read_buffer.close()
-        ark_dict[utt_id] = utt_mat
-        totframes += rows
-        if totframes>=(batch_size*buffer_size-offset):
-            break
+    return ark_dict
 
-return ark_dict,lines
+ark_dict = process_data()
+ark_delta_dict = process_data(scp_fn="data-fbank/train_si84_delta_noisy/feats.scp")
+delta_ids = sorted(ark_delta_dict.keys())
+ids = sorted(ark_dict.keys())
+mats = np.vstack([ark_dict[i] for i in ids])
+delta_mats = np.vstack([ark_delta_dict[i] for i in delta_ids])
 
-
-A = np.random.rand(1000,40)
-Process(A,0)
-
+print(mats.shape)
+print(delta_mats.shape)
+result = np.vstack([Process(mats,i) for i in range(mats.shape[0])])
+print(result[0][:10])
+print(result[0][40:50])
+print(result[0][80:90])
+print(delta_mats[0][:10])
+print(delta_mats[0][40:50])
+print(delta_mats[0][80:90])
+print(result.shape)
